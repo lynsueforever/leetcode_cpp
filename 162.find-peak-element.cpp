63fd8be31f288e1439e24de5e12d4222cbe1260c
lynsueforever <lynsueforever@163.com>
2019-02-20 16:25:45 +0800

    update 162.find-peak-element.cpp

--- 162.find-peak-element.cpp
+++ 162.find-peak-element.cpp
@@ -54,7 +54,7 @@
 [1,2] => 较大数
 [3,2,1] => 3
 6.如果mid在谷点，不好决定舍弃哪一半
-*/
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
@@ -80,5 +80,23 @@ public:
 	return -1; // remember to return
     }
 };
-
+*/
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+	int left = 0;
+	int right = nums.size() - 1;
+	
+	while (left < right) {
+		int mid = left + (right - left >> 1);
+		if (nums[mid] < nums[mid + 1]) {
+			left = mid + 1;
+		} else {
+			right = mid;
+		}
+	}
+	
+	return left;
+    }
+};
 
