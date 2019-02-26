c5ac7ee32f6110b2766ecb16a3422950737cb6a3
lynsueforever <lynsueforever@163.com>
2019-02-26 13:39:11 +0800

    update 26.remove-duplicates-from-sorted-array.0.cpp

---
+++ 26.remove-duplicates-from-sorted-array.0.cpp
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=26 lang=cpp
+ *
+ * [26] Remove Duplicates from Sorted Array
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ *
+ * algorithms
+ * Easy (39.03%)
+ * Total Accepted:    492.8K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted array nums, remove the duplicates in-place such that each
+ * element appear only once and return the new length.
+ * 
+ * Do not allocate extra space for another array, you must do this by modifying
+ * the input array in-place with O(1) extra memory.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Given nums = [1,1,2],
+ * 
+ * Your function should return length = 2, with the first two elements of nums
+ * being 1 and 2 respectively.
+ * 
+ * It doesn't matter what you leave beyond the returned length.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ * 
+ * Your function should return length = 5, with the first five elements of nums
+ * being modified to 0, 1, 2, 3, and 4 respectively.
+ * 
+ * It doesn't matter what values are set beyond the returned length.
+ * 
+ * 
+ * Clarification:
+ * 
+ * Confused why the returned value is an integer but your answer is an array?
+ * 
+ * Note that the input array is passed in by reference, which means
+ * modification to the input array will be known to the caller as well.
+ * 
+ * Internally you can think of this:
+ * 
+ * 
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ * 
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len
+ * elements.
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * 
+ */
+/*
+1.排序从小到大？
+2.不用额外空间？
+3.[1,1,2,2] => [1,2,2,1]?前面有序，然后返回长度，后面顺序和数字大小不考虑？
+4.[] 空数组返回0
+5.[1]只有1个元素返回1
+
+[1,1,2,2,3,3,4]
+len:当前非重复数组长度
+len=1
+i   : [1:n-1]
+如果array[i] != array[i-1]
+array[len++] = array[i]
+
+*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() < 2) return nums.size();
+
+	int len = 1;
+	
+	for (int i = 1; i < nums.size(); i++) {
+		if (nums[i] != nums[i - 1]) {
+			nums[len++] = nums[i];
+		}
+	}
+	
+	return len;
+    }
+};
